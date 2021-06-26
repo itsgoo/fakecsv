fc4c8057d2ab4b6c7b78b776dd7475658cc2a28c
devmashine <kurdalin.hi@gmail.com>
2021-06-26 21:36:30 +0300

    fix18

--- fakecsv_app/tasks.py
+++ fakecsv_app/tasks.py
@@ -17,21 +17,21 @@ def create_csv(numbers_of_lines, user):
         files_directory = 'files/'
         file_name = schema.title + str(schema.id) + '.csv'
         full_name = files_directory + file_name
-        
-        file_writer = csv.writer(full_name, delimiter = ",", lineterminator="\r")
-        list1 = []
-        list2 = []
-        list3 = []
-        for i in file_fields:
-            list1.append(i.column_name)
-            list2.append(i.column_type)
-            list3.append(i.parameters)
+        with open(file_name, mode="w", encoding='utf-8') as w_file:
+            file_writer = csv.writer(w_file, delimiter = ",", lineterminator="\r")
+            list1 = []
+            list2 = []
+            list3 = []
+            for i in file_fields:
+                list1.append(i.column_name)
+                list2.append(i.column_type)
+                list3.append(i.parameters)
 
-        file_writer.writerow(list1)
-        file_writer.writerow(list2)
-        file_writer.writerow(list3)
-        for i in range(int(numbers_of_lines)-3):
-            file_writer.writerow('')
+            file_writer.writerow(list1)
+            file_writer.writerow(list2)
+            file_writer.writerow(list3)
+            for i in range(int(numbers_of_lines)-3):
+                file_writer.writerow('')
 
         print('file ' + full_name + ' is done') 
 
