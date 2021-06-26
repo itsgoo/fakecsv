fdd11f452f940be96ed2a0d53c7af1d285b40b97
devmashine <kurdalin.hi@gmail.com>
2021-06-26 16:24:02 +0300

    fix broker url

--- fakecsv_project/celery.py
+++ fakecsv_project/celery.py
@@ -2,6 +2,7 @@ from celery import Celery
 
 import os
 
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fakecsv_project.settings')
 app = Celery('fakecsv_project')
 app.config_from_object('django.conf:settings', namespace='CELERY')
