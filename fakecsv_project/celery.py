68c0235af9d2380cb66f4269576f2e1a769e8a85
devmashine <kurdalin.hi@gmail.com>
2021-06-26 20:27:51 +0300

    fix10

--- fakecsv_project/celery.py
+++ fakecsv_project/celery.py
@@ -8,6 +8,10 @@ app = Celery('fakecsv_project')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
 
+app.conf.update(BROKER_URL=os.environ['REDIS_URL'],
+                CELERY_RESULT_BACKEND=os.environ['REDIS_URL'])
+
+
 @app.task(bind=True)
 def debug_task(self):
     print(f'Request: {self.request!r}')
